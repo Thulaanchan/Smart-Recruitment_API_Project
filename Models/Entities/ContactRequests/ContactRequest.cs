6d515801b71d5552e5ff3c506adb7b255bb0a9b4
Thulaanchan <kthulaanchan@gmail.com>
2026-08-10 14:18:50 +0530

    WIP checkpoint before fixing remaining errors

---
+++ Models/Entities/ContactRequests/ContactRequest.cs
@@ -0,0 +1,37 @@
+﻿using SmartRecruitmentMatchingPlatform.API.Models.Entities.Employers;
+using SmartRecruitmentMatchingPlatform.API.Models.Entities.JobSeekers;
+using SmartRecruitmentMatchingPlatform.API.Models.Enums.ContactRequests;
+
+namespace SmartRecruitmentMatchingPlatform.API.Models.Entities.ContactRequests
+{
+    public class ContactRequest
+    {
+        public int ContactRequestId { get; set; }
+
+        // Employer who sends the request
+        public int EmployerId { get; set; }
+
+        // Job seeker who receives the request
+        public int JobSeekerId { get; set; }
+
+        // Optional vacancy connected to this request
+        public int? VacancyId { get; set; }
+
+        public string? Message { get; set; }
+
+        public ContactRequestStatus Status { get; set; }
+            = ContactRequestStatus.Pending;
+
+        public DateTime CreatedAt { get; set; }
+            = DateTime.UtcNow;
+
+        public DateTime? RespondedAt { get; set; }
+
+        // Navigation properties
+        public Employer? Employer { get; set; }
+
+        public JobSeeker? JobSeeker { get; set; }
+
+        public Vacancy? Vacancy { get; set; }
+    }
+}
---
+++ Models/Enums/ContactRequests/ContactRequestStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartRecruitmentMatchingPlatform.API.Models.Enums.ContactRequests
+{
+    public enum ContactRequestStatus
+    {
+        Pending = 0,
+        Accepted = 1,
+        Declined = 2
+    }
+}
